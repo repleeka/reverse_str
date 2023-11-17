341c23b83227c8b9eddd53ba311510bcf9fba2c5
repleeka <tungondugi@gmail.com>
2023-11-17 19:11:33 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/translate', methods=['POST'])
+def translate():
+    # Dummy translation function - Replace this with your translation logic
+    data = request.get_json()
+    text_to_translate = data.get('text', '')
+    # Replace this with actual translation logic
+    translated_text = f"Translated: {text_to_translate}"
+
+    return jsonify({'translated_text': translated_text})
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
