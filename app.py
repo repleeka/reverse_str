cf8e8817a48db16bf071178efb5dd8cba794bf07
repleeka <tungondugi@gmail.com>
2023-11-17 19:51:07 +0530

    final initial codes

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    title = "Home"
+    return render_template('index.html', title=title)
 
 
 @app.route('/translate', methods=['POST'])
