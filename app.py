d66f17e96cd7513702c431326430481f0f9454cc
repleeka <tungondugi@gmail.com>
2023-11-20 01:23:03 +0530

    app.run

--- app.py
+++ app.py
@@ -18,8 +18,6 @@ def reverse():
     title = "Reverse"
     text = request.form.get('textToTranslate')
     if not text:
-        # title = "Error!!"
-        error_msg = "Empty String, enter valid string!!"
         return render_template('index.html',  title=title)
     else:
         # appending and reversing the latest entry in the list
@@ -32,3 +30,7 @@ def clear_list():
     title = "Clear"
     txt = reversed_text.clear()
     return render_template('index.html', txt=txt, title=title)
+
+
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0')
